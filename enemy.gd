f742e76e30ea2373f517669799abc2198c3f5202
ssirenkth <ssiren@kth.se>
2025-01-02 18:02:03 +0100

    feat: collision detection

--- enemy.gd
+++ enemy.gd
@@ -2,9 +2,6 @@ extends RigidBody2D
 var speed = 100
 var target_position = Vector2.ZERO
 
-
-func _init():
-	print("Enemy _init() called")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var player = get_node("../Player")
@@ -13,12 +10,16 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var current_position = global_position
 	var direction = (target_position - current_position).normalized()
 	# Move towards the target position
-	if current_position.distance_to(target_position) > 50:  # Adjust tolerance as needed
-		global_position += direction * speed * delta
-	else:
-		print("Enemy should be doing damage now.")
+	if current_position.distance_to(target_position) > 5:  # Adjust tolerance as needed
+		var collision = move_and_collide(direction)
+		if collision:
+			print("I collided with ", collision.get_collider().name)
+		
+		#global_position += direction * speed * delta
+	#else:
+		#print("Enemy should be doing damage now.")
 	pass
