1362574cd713bc738826765362eb973d845dec76
Simon Edström Kawaji <kawaji@kth.se>
2025-01-02 20:45:35 +0100

    Add UI

--- Scripts/enemy_spawner.gd
+++ Scripts/enemy_spawner.gd
@@ -3,12 +3,13 @@ extends Node2D
 @export var enemy_scenes: Array[PackedScene] = []
 @export var center: Node2D
 @export var enemy_spawn_delay = 0.1
-@export var enemy_spawn_distance = 300
+@export var enemy_spawn_distance = 200
 @export var enemies: Node2D
 
 
 var rng = RandomNumberGenerator.new()
 var current_wave_enemy_index = 0
+var current_wave = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,6 +28,8 @@ func _on_enemy_spawner_timer_timeout() -> void:
 	enemies.add_child(enemy)
 
 func _on_wave_timer_timeout() -> void:
+	current_wave += 1
+	get_node("/root/EmilScene/Control/WaveLabel").text = "Wave: " + str(current_wave)
 	if current_wave_enemy_index < enemy_scenes.size() - 1:
 		current_wave_enemy_index += 1
 	else:
