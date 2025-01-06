2b4b4c833e6fa702359aa61c7cb3c7de21051919
Daniel Parhizgar <daniel.parhizgar@gmail.com>
2025-01-06 18:12:20 +0100

    ThunderStrike each 5 seconds

--- Scripts/Weapons/ThunderStrike.gd
+++ Scripts/Weapons/ThunderStrike.gd
@@ -2,9 +2,16 @@ extends BaseGun
 
 class_name ThunderStrike
 
-#TODO: Get random area around player
-func get_target_area() -> Vector2: #defaults to getting closest
-	return player.global_position
+#TODO: Get random screen position
+func get_target_area() -> Vector2:
+	var screenSize = get_viewport().get_visible_rect().size;
+	var rng = RandomNumberGenerator.new()
+	var playerPos = player.global_position
+	var xOffset = 0.0#abs(playerPos.x - screenSize.y) * 0.8;
+	var rndX = rng.randi_range(playerPos.x - screenSize.y/2.0 + xOffset, playerPos.x + screenSize.y/2.0 - xOffset)
+	var rndY = rng.randi_range(0, screenSize.y)
+	var position = Vector2(rndX,rndY);
+	return position
 	
 #TODO: Spawn lighting strike!
 func shoot_area(position: Vector2) -> void:
