f21d47a6bf29a996b0bac995e2d4aa6ab44120c9
Daniel Parhizgar <daniel.parhizgar@gmail.com>
2025-01-08 17:48:33 +0100

    More orb variants added and push out orb upgrade added

--- Scripts/Weapons/fire_orb.gd
+++ Scripts/Weapons/fire_orb.gd
@@ -2,23 +2,30 @@ extends BaseGun
 
 class_name FireOrb
 
-var fireOrbRange = 0.0; #10 feets away from player
+var orbRange = 0.0; #10 feets away from player
 var currAngle = 0.0;
 var nextAngle = 0.0;
 var nextPos;
 var orbSpeed = 1.0;
+var damage_per_tick = 200.0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	player = get_node("../..")
 	player.addPlayerFireOrb(self);
 	var screenSize = get_viewport().get_visible_rect().size;
-	fireOrbRange = screenSize.y*0.15; #10% of the screenSize, to make this scale properly
+	orbRange = screenSize.y*0.15; #10% of the screenSize, to make this scale properly
 	pass # Replace with function body.
+
+func _physics_process(delta):
+	for body in $Area2D.get_overlapping_bodies():
+		if body is Enemy:
+			body.take_damage(damage_per_tick * delta)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#Rotate fireorb here
 	currAngle = currAngle + delta * orbSpeed;
-	var orbPos = Vector2(cos(currAngle), sin(currAngle))*fireOrbRange;
+	var orbPos = Vector2(cos(currAngle), sin(currAngle))*orbRange;
 	orbPos += player.global_position;
 	global_position = orbPos;
