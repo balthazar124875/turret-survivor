71b646b66a7439623c63c8796a8ed3f990d6f6ba
Daniel Parhizgar <daniel.parhizgar@gmail.com>
2025-03-29 23:17:57 +0000

    Vine wall first version working, damages enemies, roots them

--- Scripts/vine_wall_bullet.gd
+++ Scripts/vine_wall_bullet.gd
@@ -1,17 +1,37 @@
 extends Node2D
 
-#TODO: Here the vinewall will damage enemies, animate etc, die etc
-var hp : int;
+class_name VineBullet
 
-func instantiateVineWall(hpx : int) -> void:
+#TODO: Here the vinewall will damage enemies, animate etc, die etc
+var hp : float;
+var attackPower : float;
+
+func instantiateVineWall(hpx : float, atkDmg : float) -> void:
 	hp = hpx;
+	attackPower = atkDmg;
 	pass
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+#TODO: This needs to slow enemies
+func _physics_process(delta):
+	for body in $Area2D.get_overlapping_bodies():
+		if body is Enemy:
+			HitEnemy(body, delta);
+
+func take_damage(damage : int, enemy : Enemy):
+	hp -= damage;
+	enemy.take_damage(attackPower);
+
+func HitEnemy(body, delta) -> void:
+	if body.GetObjectObstructingEnemy() == null:
+		body.SetObjectObstructingEnemy(self);
+	return;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if hp <= 0:
+		queue_free();
 	pass
