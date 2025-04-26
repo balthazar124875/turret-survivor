6c21a22290d6bc45766e8d54538af80c099c8722
Simoid <simonkawaji@gmail.com>
2025-04-26 15:56:48 +0200

    finish poison nova

--- Scripts/Weapons/poison_nova_weapon.gd
+++ Scripts/Weapons/poison_nova_weapon.gd
@@ -3,34 +3,44 @@ extends BaseGun
 class_name PoisonNova
 
 @export var particle_effect: PackedScene
+@onready var area2d = $Area2D
+@onready var collision_shape = $Area2D/CollisionShape2D
 
-func shoot(enemy: Node) -> void:	
-	if player.global_position.distance_to(get_target().global_position) > range * player.rangeMultiplier:
-		return
-	var bullet = bullet.instantiate()
-	if get_target() != null:
-		bullet.target_pos = get_target().global_position - player.global_position
-	bullet.speed = base_projectile_speed
-	bullet.damage = damage * player.damageMultiplier * gun_damage_multiplier
-	bullet.cluster_times = cluster_times + player.extraChains if recursive else cluster_times
-	bullet.number_of_cluster_bombs = base_projectile_amount + player.extraProjectiles
-	add_child(bullet)
+@export var base_radius = 230
+
+var poison_duration = 4
+var poison_magnitude = 1
+
+func shoot(enemy: Node) -> void:
+	poison_enemies_in_collider()
+	collision_shape.shape.radius = base_radius * player.areaSizeMultiplier
+	var new_particles = particle_effect.instantiate()
+	add_child(new_particles)
 	
 func apply_level_up():
 	if(level == 5):
-			base_projectile_amount += 1
+			poison_magnitude += 2
 			return
 	if(level == 10):
-			recursive = true
-			cluster_times += 1
+			poison_duration += 2
 			return
 	
 	match level % 5:
 		1:
-			base_projectile_speed += 50
+			base_radius += 20
 		2:
 			cooldown *= 0.95
 		3:
-			range += 25
+			poison_duration += 1
 		4:
-			damage += 1
+			poison_magnitude += 1
+
+func poison_enemies_in_collider():
+	var enemies = []
+	for body in area2d.get_overlapping_bodies():
+		if body is Enemy:  # Check if it's an enemy
+			var poisonEffect = EnemyStatusEffect.new()
+			poisonEffect.type = GlobalEnums.ENEMY_STATUS_EFFECTS.POISONED
+			poisonEffect.duration = poison_duration
+			poisonEffect.magnitude = poison_magnitude
+			body.apply_status_effect(poisonEffect)
