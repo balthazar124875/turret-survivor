18ff4192a707b0f2f105fb82303efe208bb1250a
Daniel Parhizgar <daniel.parhizgar@gmail.com>
2025-04-29 07:39:50 +0100

    Flamethrower collider working now (instantly kills enemies)

---
+++ Scripts/PlayerProjectiles/flamethrower_collider.gd
@@ -0,0 +1,99 @@
+extends Area2D
+
+var flameMaxRange;
+var currFlameRange;
+var innerPtsOffset; #The pts starting at player pos, these will grow as the flame turns off and closes
+var outerGrowSpeed;
+var innerGrowSpeed; #Should be slightly slower than outer grow speed
+var isGrowing;
+var spread_angle;
+var speedScale;
+
+#Lower value here makes the cone shape sharper
+#We want to increase this value towards 1 as the flame turns off
+var coneShapeIntensityScale = 0.5;
+var minConeShapeIntensity = 0.5;
+var damage = 0.0;
+
+var collider : CollisionPolygon2D;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	monitoring = true
+	monitorable = true;
+	outerGrowSpeed = 0.55;
+	innerGrowSpeed = 0.4;
+	speedScale = 1.0;
+	coneShapeIntensityScale = 0.5;
+	minConeShapeIntensity = 0.5;
+	currFlameRange = 0.0;
+	isGrowing = true;
+	innerPtsOffset = 0.0;
+	collider = get_node("CollisionPolygon2D");
+	collider.disabled = false
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#This will make sure our collider outer points are growing
+	currFlameRange += outerGrowSpeed * delta * speedScale;
+	if(currFlameRange >= flameMaxRange):
+		currFlameRange = flameMaxRange;
+		
+	#This will make the inner points (pts close to player) close in.
+	if !isGrowing:
+		innerPtsOffset += innerGrowSpeed * delta * speedScale;
+		var ratio = innerPtsOffset / flameMaxRange; #If ratio 1 it means the inner has reached outer and collider has disappeared
+		#Interpolate
+		coneShapeIntensityScale = minConeShapeIntensity * (1 - ratio) + 1.0 * ratio;
+	
+	if innerPtsOffset >= flameMaxRange:
+		queue_free(); #Destroy when the collider fully disappears which happens when the flame disappears
+	
+	#UpdateSingleFlameColliderPoints()
+	pass
+
+func init(maxFlameRange : float, spreadAngle : float, flameDamage : float, emitterScaleIncrease : float) -> void:
+	flameMaxRange = maxFlameRange;
+	spread_angle = spreadAngle;
+	damage = flameDamage;
+	speedScale *= emitterScaleIncrease; #The particle speed become faster as the scale increase
+	pass
+
+func UpdateData(maxFlameRange : float, spreadAngle : float, flameDamage : float, emitterScaleIncrease : float) -> void:
+	init(maxFlameRange, spreadAngle, flameDamage, emitterScaleIncrease);
+	pass
+	
+func TurnOff() -> void:
+	isGrowing = false;
+
+func UpdateSingleFlameColliderPoints() -> void:
+	var colRange = currFlameRange * 250;
+	var half_width = colRange * tan(deg_to_rad(spread_angle) / 2)
+	var innerPts = innerPtsOffset * 250;
+	var points = [
+		Vector2(innerPts, -half_width*coneShapeIntensityScale), # Top-left
+		Vector2(colRange, -half_width*1.1),       # Top-right
+		Vector2(colRange, half_width*1.1),        # Bottom-right
+		Vector2(innerPts, half_width*coneShapeIntensityScale)   # Bottom-left
+	]
+	collider.polygon = points;
+	collider.disabled = false #This is apparently needed to make Godot refresh the shape for collision check
+	#Also super important after refreshing the collider:
+	#YOU MUST Wait at least one physics frame before checking for overlaps
+	#For this you need to use await get_tree().physics_frame.
+	#await get_tree().physics_frame
+	#await Engine.get_main_loop().process_frame
+	#await get_tree().process_frame
+
+func HitEnemy(body) -> void:
+	body.take_damage(damage*100.0, "Flamethrower", GlobalEnums.DAMAGE_TYPES.FIRE)
+
+func _physics_process(delta):
+	UpdateSingleFlameColliderPoints();
+
+func _on_body_entered(body: Node2D) -> void:
+	#TODO: Store enemies in a list and make them take tick damage
+	if body is Enemy:
+		HitEnemy(body);
+	pass # Replace with function body.
--- Scripts/Weapons/FlameThrower.gd
+++ Scripts/Weapons/FlameThrower.gd
@@ -3,12 +3,12 @@ extends BaseGun
 class_name FlameThrower
 
 @export var flameThrowerVfx : PackedScene
+@export var flameCollider : PackedScene
 var flame_thrower_instance : Node2D
 var ftParticles : GPUParticles2D
 var IsShooting : bool;
 var targettedEnemy : Node2D;
 
-@onready var flameCollider;
 var areaSizeMultiplierScale = 0.375;
 var enemy_to_flameColliders_hashMap = {};
 var deadFlameColliders = []; #Colliders that are about to die, will shrink and get deleted
@@ -50,46 +50,21 @@ func _process(delta: float) -> void:
 	elif(targettedEnemy == null):
 		IsShooting = false;
 		StopFlameThrowerVfx();
-	UpdateAllFlameColliderPoints();
-	UpdateDeadFlameColliderPoints();
 	pass
 
 func CreateFlameColliderInstance(enemyHash : Enemy, directionAngle : float) -> void:
 	#TODO: Create a new flameCollider instance and store in list
-	var flameColliderInst = flame_thrower_instance.get_node("Area2D").get_node("CollisionPolygon2D").duplicate();
-	flameColliderInst.rotation = directionAngle;
+	var flameColliderInst = flameCollider.instantiate();
 	flame_thrower_instance.add_child(flameColliderInst);
+	flameColliderInst.rotation = directionAngle;
+	flameColliderInst.init(flameColRange, spread_angle, damage * player.damageMultiplier, flameRange / ogFlameRange);
 	enemy_to_flameColliders_hashMap[enemyHash] = flameColliderInst;
-	UpdateFlameAreaSize()
 
 func DestroyFlameColliderInstance(enemyHash: Enemy) -> void:
 	var flameCollider = enemy_to_flameColliders_hashMap.get(enemyHash, null)
 	if flameCollider:
 		enemy_to_flameColliders_hashMap.erase(enemyHash);
-		deadFlameColliders.push_back(flameCollider);
-		flameCollider.queue_free(); #NEED REWORK!!
-	pass
-
-func UpdateAllFlameColliderPoints() -> void:
-	for enemyHash in enemy_to_flameColliders_hashMap:
-		var flameCollider = enemy_to_flameColliders_hashMap[enemyHash];
-		UpdateSingleFlameColliderPoints(flameCollider)
-	pass
-	
-func UpdateSingleFlameColliderPoints(flameCollider : Node2D) -> void:
-	var colRange = flameColRange * 250;
-	var half_width = colRange * tan(deg_to_rad(spread_angle) / 2)
-	var points = [
-		Vector2(0, -half_width*0.5),           # Top-left
-		Vector2(colRange, -half_width*1.1),    # Top-right
-		Vector2(colRange, half_width*1.1),     # Bottom-right
-		Vector2(0, half_width*0.5)             # Bottom-left
-	]
-	flameCollider.polygon = points
-	
-func UpdateDeadFlameColliderPoints() -> void:
-	#for flameCollider in deadFlameColliders:
-	#	flameCollider.queue_free();
+		flameCollider.TurnOff() #Will automatically shrink and free the collider.
 	pass
 		
 func IncreaseFlameAreaSizeMultiplier(stat: GlobalEnums.PLAYER_STATS, new_total: float, increase: float) -> void:
@@ -101,6 +76,30 @@ func UpdateFlameAreaSize() -> void:
 	spread_angle = 20 + (multiplier - 1.0)*15; #Increase this with scale increase
 	finalFlameScale = currentMinParticleScale * multiplier;
 	ftParticles.process_material.scale = finalFlameScale;
+	UpdateCollidersData()
+
+#Send the new flame range and spread_angle to the colliders
+func UpdateCollidersData() -> void:
+	for enemyHash in enemy_to_flameColliders_hashMap:
+		var flameCollider = enemy_to_flameColliders_hashMap[enemyHash];
+		flameCollider.UpdateData(flameColRange, spread_angle, damage * player.damageMultiplier, flameRange / ogFlameRange);
+	pass
+	
+#adjust the flamethrower vfx visuals based on range
+func IncreaseFlameThrowerRange(range : float) -> void:
+	flameRange += range;
+	if(flameRange >= maxFlameRange):
+		flameRange = maxFlameRange;
+		
+	ftParticles.scale = Vector2(flameRange, flameRange);
+	var rangeRatio : int = flameRange / ogFlameRange; #How many percentage did the range increase with?
+
+	#Increase amount of particles
+	ftParticles.amount = 100*rangeRatio;
+	flameColRange = flameRange*0.9;
+	#TODO: We might need to destroy all active colliders since updating scale resets the animation
+	UpdateCollidersData()
+	pass
 
 func PlayFlameThrowerVfx(enemy : Node2D) -> void:
 	#Rotate properly
@@ -114,24 +113,11 @@ func StopFlameThrowerVfx() -> void:
 	ftParticles.emitting = false;
 	pass
 	
-#adjust the flamethrower vfx visuals based on range
-func IncreaseFlameThrowerRange(range : float) -> void:
-	flameRange += range;
-	if(flameRange >= maxFlameRange):
-		flameRange = maxFlameRange;
-		
-	ftParticles.scale = Vector2(flameRange, flameRange);
-	var rangeRatio : int = flameRange / ogFlameRange;
-
-	#Increase amount of particles
-	ftParticles.amount = 100*rangeRatio;
-	flameColRange = flameRange*0.9;
-	pass
-	
 func apply_level_up():
 	
 	if(level > 10):
 		damage *= 1.2;
+		UpdateCollidersData()
 		return;
 	
 	if(level == 5):
@@ -139,14 +125,14 @@ func apply_level_up():
 		UpdateFlameAreaSize()
 		IncreaseFlameThrowerRange(0.5);
 		damage *= 1.5
+		UpdateCollidersData()
 		return
 	if(level == 10):
-		#ftParticles.scale += Vector2(1.0,1.0);
-		#ftParticles.amount += 100;
 		currentMinParticleScale *= 1.5
 		UpdateFlameAreaSize()
 		IncreaseFlameThrowerRange(0.5);
 		damage *= 2.0
+		UpdateCollidersData()
 		return
 	
 	match level % 5:
@@ -154,11 +140,9 @@ func apply_level_up():
 			damage *= 1.5
 		2:
 			IncreaseFlameThrowerRange(0.5);
-			#ftParticles.scale += Vector2(0.5,0.5);
-			#ftParticles.amount += 75;
 		3:
 			damage *= 1.5
 		4:
 			IncreaseFlameThrowerRange(0.5);
-			#ftParticles.scale += Vector2(0.5,0.5);
-			#ftParticles.amount += 75;
+			
+	UpdateCollidersData()
