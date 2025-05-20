a8b7df1075f165b17f1cd4b0d432b2798481f9ea
Simoid <simonkawaji@gmail.com>
2025-05-20 21:52:04 +0200

    add 1 background

--- Scripts/background_handler.gd
+++ Scripts/background_handler.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 
 var active_child: int = 0
-
+@onready var number_of_backgrounds: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	SignalBus.current_wave_updated.connect(update_background)
+	number_of_backgrounds = get_child_count()
 	pass # Replace with function body.
 
 
@@ -16,7 +17,7 @@ func _process(delta: float) -> void:
 func update_background(wave: int):
 	if(wave % 10 == 0):
 		var c1 = get_child(active_child)
-		var c2 = get_child((active_child + 1) % 2)
+		var c2 = get_child((active_child + 1) % number_of_backgrounds)
 		c1.start_disolve(1, -1)
 		c2.reset()
-		active_child = (active_child + 1) % 2
+		active_child = (active_child + 1) % number_of_backgrounds
